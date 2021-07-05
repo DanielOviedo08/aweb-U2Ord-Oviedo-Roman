47b58ee314e6966a1768d79b0be34a7bb00efc6d
Daniel Oviedo <20203tn140@utez.edu.mx>
2021-07-05 11:00:32 -0500

    Examen u2

---
+++ JS/mi-app.js
@@ -0,0 +1,11 @@
+var newNote=document.createElement("textarea");
+newNote.setAttribute("id","button");
+newNote.setAttribute("onclick","Guardarn()");
+
+document.body.appendChild(newNote);
+
+function Guardarn(){
+    
+  
+}
+
